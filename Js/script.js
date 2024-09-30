6ee8bf8a65a631a4fbcdad735ce59bdd600ee8bb
Fauziahalsey <fauziaomala19@gmail.com>
2024-09-30 10:35:40 +0300

    making initial changes

---
+++ Js/script.js
@@ -0,0 +1,39 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY >0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navbar.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+	distance: '25px',
+	duration: 2500,
+	reset: true
+})
+
+
+sr.reveal('.home-text',{delay:190, origin:'bottom'})
+
+sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
+// First, get a reference to the image.
+const image = document.querySelector('img');
+
+// Then, define a function that toggles a "glow" CSS class.
+function toggleGlow() {
+  image.classList.toggle('glow');
+}
+
+// Finally, add an event listener that toggles the class when the image is clicked.
+image.addEventListener('click', toggleGlow);
